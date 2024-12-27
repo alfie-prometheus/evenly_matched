0d5ba7e89d56399bfcaf8753a384d353b163b7cf
Alfie Jaiyoon Kim <72483011+alfie-prometheus@users.noreply.github.com>
2024-12-26 21:12:02 -0500

    Initial commit
    Initial commit for evenly matched

---
+++ README.md
@@ -0,0 +1,2 @@
+# Evenly_matched
+ "Evenly Matched - A JavaScript Looping Exercise" A simple coding project that displays numbers from 0 to 100, highlighting even numbers in bold. Built with HTML, Bootstrap, and JavaScript to practice loops and basic DOM manipulation. Users input start and end values to generate results.
---
+++ site.js
@@ -0,0 +1,67 @@
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //get values from the page 
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+    //Validate input
+    //Parse into integer)
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers
+        displayNumbers(numbers);     
+
+    }else{
+        alert("You must enter integers.");
+    }
+
+
+}
+
+//generate numbers from startvalue to the endvalue
+//logic function(s)
+function generateNumbers(sValue, eValue){
+
+    let numbers = [];
+
+    //get all numbers from the start to the end
+    for (let i = sValue; i <= eValue; i++) {
+
+        //this will execute until the i(index) gets the eValue
+        numbers.push(i);
+        
+    }
+
+    return numbers;
+
+}
+
+//display the numbers and mark even numbers bold
+//display or view functions
+function displayNumbers(numbers){
+
+    let templateRows="";
+
+    for (let index = 0; index < numbers.length; index++) {
+        
+        let className = "even";
+
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+        }
+        else{
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+}
+
